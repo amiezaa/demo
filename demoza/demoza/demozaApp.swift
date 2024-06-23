8fb01ab5a8bd0dc3f5efc7081e3ed185991e3b5c
amiezaa <amiehem@gmail.com>
2024-06-23 10:07:49 +0700

    init project

---
+++ demoza/demoza/demozaApp.swift
@@ -0,0 +1,17 @@
+//
+//  demozaApp.swift
+//  demoza
+//
+//  Created by amie on 23/6/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct demozaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
